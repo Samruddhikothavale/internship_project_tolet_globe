9fcbaebb432dc6ec2e707157febbaffebc6afd76
Samruddhikothavale <samruddhikothavale23@gmail.com>
2025-05-29 21:15:31 +0530

    Updated about page or other changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
-import { About } from "./pages/about";
+import { About } from "./pages/About";
 import { Blogs } from "./pages/Blogs";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
