92e85d6d009bad32c989cfd1f78d2ea7514682f5
Samruddhikothavale <samruddhikothavale23@gmail.com>
2025-05-30 14:52:22 +0530

    changed root dir address

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/form" , contactRouter);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 // app.get("/",(req,res)=>{
