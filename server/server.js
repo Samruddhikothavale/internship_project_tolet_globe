77a8cd22f52a3ff8dd2d10bc19801075e68e705a
Samruddhikothavale <samruddhikothavale23@gmail.com>
2025-05-30 15:05:11 +0530

    changed url for client

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const authRouter =require("./router/auth-router");
 const contactRouter =require("./router/contact-router");
 const connectdb = require("./utils/db");
 const errormiddleware = require("./middelware/errorhandler_middelware");
-const path = require("path");
+
+const CLIENT_URL = process.env.CLIENT_URL;
 
 
 var corsOptions = {
-  origin: "http://localhost:5173",
+  origin: CLIENT_URL,
   methods:"GET, POST , PUT , DELETE , HEAD ",
   credential:true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -22,11 +23,7 @@ app.use(express.json());
 app.use("/api/auth" , authRouter);
 app.use("/api/form" , contactRouter);
 
-app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
 
 // app.get("/",(req,res)=>{
 //     res.status(200).send('Welcome to pet shop');
