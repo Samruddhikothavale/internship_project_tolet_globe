9d59cb983b04f6a260d7d9bdb88ab9c3925126e4
Samruddhikothavale <samruddhikothavale23@gmail.com>
2025-06-08 17:59:04 +0530

    typos removed

--- client/src/pages/Blogs/Blogs.jsx
+++ client/src/pages/Blogs/Blogs.jsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
-import { SearchBlog } from "./SearchBlog"
-import "./blogs.css";
+// import { useState } from "react";
+// import { SearchBlog } from "./SearchBlog"
+// import "./blogs.css";
 
 
-export const Blogs =()=>{
-    const [search , setSearch] = useState('');
-    const [catergory , setcatergory] = useState('');
+// export const Blogs =()=>{
+//     const [search , setSearch] = useState('');
+//     const [catergory , setcatergory] = useState('');
 
-    const [query , setquery] = useState({search:"" ,catergory:""});
+//     const [query , setquery] = useState({search:"" ,catergory:""});
 
 
-    const handleSearchchange = (e) =>{
-        setSearch(e.target.value);
-    }
+//     const handleSearchchange = (e) =>{
+//         setSearch(e.target.value);
+//     }
 
-    const handleSearch = () =>{
+//     const handleSearch = () =>{
 
-    }
-    return (
-        <>
-        <div className="mt-16 container mx-auto">
-            <SearchBlog/>
-        </div>
+//     };
+//     return (
+//         <>
+//         <div className="mt-16 container mx-auto">
+//             <SearchBlog/>
+//         </div>
         
-        </>
-    )
- }
+//         </>
+//     )
+//  };
